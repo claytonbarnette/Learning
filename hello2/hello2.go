7b23e84578ca2018c8da0e069cab480a3fe52063
Clayton Barnette <claytonbarnette@gmail.com>
2019-02-02 11:17:33 +0000

    Expample Hello, World Go Programs

---
+++ hello2/hello2.go
@@ -0,0 +1,20 @@
+// This version imports the stringutil library expample from the Go website
+// https://golang.org/doc/code.html 
+
+package main
+
+import (
+
+	"fmt"
+	"github.com/claytonbarnette/hello/stringutil"
+
+)
+
+func main(){
+
+	//fmt.Println("Hello, World")
+
+	fmt.Println(stringutil.Reverse("!gnimmargorP oG ,olleH"))
+
+}
+
